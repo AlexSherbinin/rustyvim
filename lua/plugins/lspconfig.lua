8bfd02c7e9242f0edfca1d61ff3f011023d74c19
AlexSherbinin <alex.sherbinin.andr@gmail.com>
2024-05-03 17:26:07 +0300

    Fixed deprecation message on legacy vim.lsp.inlay_hint.enable usage

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,21 +1,16 @@
 return {
-  'neovim/nvim-lspconfig',
-  config = function()
+	'neovim/nvim-lspconfig',
+	config = function()
 		local utils = require('utils')
 		local mappings = require('core.mappings')
-    require('neodev').setup {}
+		require('neodev').setup {}
 
 		vim.api.nvim_create_autocmd('LspAttach', {
-			callback = function (ev)
+			callback = function(ev)
 				local buffer = ev.buf
 				utils.map_key_valued('n', mappings.lsp, vim.lsp.buf, { buffer = buffer })
 
-				local client = vim.lsp.get_client_by_id(ev.data.client_id)
-
-				---@diagnostic disable-next-line: need-check-nil
-				if client.server_capabilities.inlayHintProvider then
-					vim.lsp.inlay_hint.enable(buffer, true)
-				end
+				vim.lsp.inlay_hint.enable()
 			end
 		})
 
